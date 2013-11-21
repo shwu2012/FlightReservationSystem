a79315892ccca23d1fb09f5c99f27ad120d3e305
Shuang Wu <shuangwu2011@gmail.com>
2013-11-21 14:38:47 -0800

    logout servlet

--- src/sjsu/cs157a/dbpro/servlet/LogoutServlet.java
+++ src/sjsu/cs157a/dbpro/servlet/LogoutServlet.java
@@ -1,8 +1,8 @@
 package sjsu.cs157a.dbpro.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,14 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getSession().removeAttribute("username");
 		request.getSession().invalidate();
