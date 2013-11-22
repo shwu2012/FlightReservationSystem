1a68af9f40441f332a05e28f1a10dcd92a8a06ff
janejeakcy <janehuangjing2008@hotmail.com>
2013-11-21 22:15:47 -0800

    cancel reservation

---
+++ WebContent/WEB-INF/jsp/cancelReservation.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Passenger Dashboard</title>
+    <meta charset="UTF-8" />
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
+<body>
+    <div id="main" class="center">
+        <div id="header">
+            <h1>Passenger Dashboard</h1>
+            <div id="welcome">
+                <span>Welcome, </span>
+                <span id="userName"><c:out value="${username}"/></span>
+            </div>
+            <div id="logout">
+                <a id="userLogout" href="logout">Logout</a>
+            </div>
+        </div>
+        <div id="menu">
+            <ul id="menuItems">
+                <li class="menuOption" id="vc"><a href="passengerdashboard?show=viewCustomerInfo">View contact info</a></li>
+                <li class="menuOption" id="ec"><a href="passengerdashboard?show=editPassengerInfo">Edit contact info</a></li>
+                <li class="menuOption" id="mr"><a href="passengerdashboard?show=makeReservation">Make a reservation</a></li>
+                <li class="menuOption" id="cr"><a href="passengerdashboard?show=cancelReservation">Cancel a reservation</a></li>
+                <li class="menuOption" id="va"><a href="passengerdashboard?show=viewAirports">View airports</a></li>
+                <li class="menuOption" id="vaa"><a href="passengerdashboard?show=viewAirlinesPerAirport">View airlines per airport</a></li>
+            </ul>
+        </div>
+        <div id="contentArea">
+            <div id="cancelreservation">
+                <h2>Cancel Reservation</h2>
+                <p>Reservation canceled successfully.<p>              
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ src/sjsu/cs157a/dbpro/servlet/CancelReservationServlet.java
@@ -0,0 +1,98 @@
+package sjsu.cs157a.dbpro.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import sjsu.cs157a.dbpro.db.DbConnection;
+
+/**
+ * Servlet implementation class cancelReservationServlet
+ */
+@WebServlet("/cancelReservationServlet")
+public class CancelReservationServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger
+			.getLogger(CancelReservationServlet.class);
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+
+		String puserName = (String) req.getSession().getAttribute("username");
+
+		String reservationID = req.getParameter("reservationNumber");
+		
+		logger.info("reservationID: " + reservationID);
+		
+		Connection conn = DbConnection.openConnection();
+
+		PreparedStatement prepStmt = null;
+
+		String sql = "delete from reservation " + "where reservationID = ?";
+
+		// logger.info("write update sql");
+
+		try
+		{
+			prepStmt = conn.prepareStatement(sql);
+			prepStmt.setString(1, reservationID);
+			// test sql
+			logger.info("prepStmt: " + prepStmt.toString());
+
+			prepStmt.executeUpdate();
+
+		}
+		catch (SQLException se)
+		{
+			se.printStackTrace();
+			// sqlError = se.getMessage();
+
+		}
+		finally
+		{
+			try
+			{
+				prepStmt.close();
+			}
+			catch (SQLException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		DbConnection.closeConnection(conn);
+
+		// logger.info("close connection");
+
+		logger.info("Username: " + puserName);
+		/*
+		 * if (sqlError == null) {
+		 * req.getRequestDispatcher("/WEB-INF/jsp/editContactInfo.jsp"
+		 * ).forward(req, resp); } else { req.setAttribute("sqlError",
+		 * sqlError); req.setAttribute("type", "registerPassenger");
+		 * req.getRequestDispatcher
+		 * ("/WEB-INF/jsp/passengerDashboard.jsp").forward(req, resp); }
+		 */
+
+		req.getRequestDispatcher("/WEB-INF/jsp/cancelReservation.jsp").forward(
+				req, resp);
+	}
+
+}
